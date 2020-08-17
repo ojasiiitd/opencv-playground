8a6ae7fc52d0d2edf16692f97bc1c58808a46486
Ojas Saxena <ojassaxena03@gmail.com>
2020-08-17 20:43:54 +0530

    erosion and dilation techniques

--- filtering colors/filtering.py
+++ filtering colors/filtering.py
@@ -19,18 +19,30 @@ while True:
     # removing noise (blur techniques)
     blurBlock = 5
     kernel = np.ones((blurBlock,blurBlock))/(blurBlock**2)
-    smoothed = cv2.filter2D(res , -1 , kernel)
+    # smoothed = cv2.filter2D(res , -1 , kernel)
 
-    blur = cv2.GaussianBlur(res , (blurBlock,blurBlock) , 0)
+    # blur = cv2.GaussianBlur(res , (blurBlock,blurBlock) , 0)
 
-    median = cv2.medianBlur(res , blurBlock)
+    medianRes = cv2.medianBlur(res , blurBlock)
 
-    bilat = cv2.bilateralFilter(res , blurBlock , 75 , 75)
+    # bilat = cv2.bilateralFilter(res , blurBlock , 75 , 75)
 
     # cv2.imshow("smooth blur" , smoothed)
     # cv2.imshow("guass blur" , blur)
-    cv2.imshow("median blur" , median)
+    cv2.imshow("median blur" , medianRes)
     # cv2.imshow("bilat blur" , bilat)
+
+    # erosion, dilation, opening, closing
+    slider = np.ones((7,7))
+    erosion = cv2.erode(mask , slider , iterations=2) # erodes erroe
+    dilation = cv2.dilate(mask , slider , iterations=1) # dilates error (check imshow)
+    opening = cv2.morphologyEx(mask , cv2.MORPH_OPEN , slider) # removes false positives
+    closing = cv2.morphologyEx(mask , cv2.MORPH_CLOSE , slider) # removes false negatives
+
+    # cv2.imshow("erode" , erosion)
+    # cv2.imshow("dilate" , dilation)
+    # cv2.imshow("open" , opening)
+    # cv2.imshow("close" , closing)
     
     # exit
     if cv2.waitKey(1) & 0xFF == ord('q'):
