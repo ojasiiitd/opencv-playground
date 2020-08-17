479550c99b9d275329e43d50b28428ac2eb0d80e
Ojas Saxena <ojassaxena03@gmail.com>
2020-08-17 21:09:15 +0530

    edge detection

--- filters and edges/edge.py
+++ filters and edges/edge.py
@@ -9,12 +9,12 @@ while True:
     cv2.imshow("org" , frame)
 
     # gradient and edge techniques
+    
     # laplate = cv2.Laplacian(frame , cv2.CV_64F)
     # sobelx = cv2.Sobel(frame , cv2.CV_64F , 1 , 0 , ksize=5)
     # sobely = cv2.Sobel(frame , cv2.CV_64F , 0 , 1 , ksize=5)
     edge = cv2.Canny(frame , 90 , 90) # more imp edges, threshold = big
 
-    # cv2.imshow("laplated" , laplate)
     # cv2.imshow("laplated" , laplate)
     # cv2.imshow("sobel x" , sobelx)
     # cv2.imshow("sobel y" , sobely)
